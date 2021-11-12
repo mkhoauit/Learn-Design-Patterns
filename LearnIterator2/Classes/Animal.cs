2bd26d8e2e0d607058d979d0d80497226a92f39b
mkhoauit <92783675+mkhoauit@users.noreply.github.com>
2021-11-12 15:37:19 +0700

    Learn Iterator

---
+++ LearnIterator2/Classes/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnIterator2.Classes
+{
+     class Animal
+    {
+        public enum TypeEnum { Bird, Mammal }
+
+        public string Name { get; set; }
+        public TypeEnum Type { get; set; }
+    }
+}
---
+++ LearnIterator2/Program.cs
@@ -0,0 +1,62 @@
+﻿using LearnIterator2.Classes;
+using System;
+
+namespace LearnIterator2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zoo zoo = new Zoo();
+
+            zoo.AddMammal("Whale");
+            zoo.AddMammal("Rhinoceros");
+            zoo.AddBird("Penguin");
+            zoo.AddBird("Warbler");
+
+            Console.WriteLine("Your zoo have: ");
+            foreach (string name in zoo)
+            {
+                Console.Write($"| {name} | ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Choose a number [1 or 2 or another number to skip]:");
+            Console.WriteLine("1: Add a Mammal");
+            Console.WriteLine("2: Add a Bird");
+            uint input = Convert.ToUInt32(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("Input Mammal's Name:");
+                    string mammalName = Convert.ToString(Console.ReadLine());
+                    zoo.AddMammal(mammalName);
+                    break;
+                case 2:
+                    Console.WriteLine("Input Bird's Name:");
+                    string birdName = Convert.ToString(Console.ReadLine());
+                    zoo.AddMammal(birdName);
+                    break;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Birds");
+            foreach (string name in zoo.Birds)
+            {
+                
+                Console.Write($"| {name} | ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Mammals");
+            foreach (string name in zoo.Mammals)
+            {
+                
+                Console.Write($"| {name} | ");
+            }
+            Console.WriteLine();
+       
+        }
+    }
+}
